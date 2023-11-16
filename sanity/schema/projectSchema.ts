0954a22557054f4f1485bec0d78c9026c2f63ce0
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-11-16 16:15:10 +0100

    gallery added date set

--- sanity/schema/projectSchema.ts
+++ sanity/schema/projectSchema.ts
@@ -6,46 +6,53 @@ export const project = {
 };
 
 export const gallery = {
-    name: 'gallery',
-    title: 'Gallery',
-    type: 'document',
-    fields: [
-        {
-            name: 'description',
-            title: 'Description',
-            type: 'string',
-        },
-        
-        {
-            name: 'image',
-            title: 'Select Image',
-            type: 'image',
-            options: {
-                hotspot: true,
-            }
-        }
-    ]
-}
+  name: "gallery",
+  title: "Gallery",
+  type: "document",
+  fields: [
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "date",
+      title: "date",
+      type: "date",
+      options: {
+        dateFormat: "YYYY-MM-DD",
+      },
+    },
+    {
+      name: "image",
+      title: "Select Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
 
 export const speeches = {
-    name: "speeches",
-    title: "Speeches",
-    type: "document",
-    fields: [
+  name: "speeches",
+  title: "Speeches",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "speech",
+      title: "Speech",
+      type: "array",
+      of: [
         {
-            name: "title",
-            title: "Title",
-            type: "string",
+          type: "block",
         },
-        {
-            name: "speech",
-            title: "Speech",
-            type: "array",
-            of: [
-                {
-                    type: "block",
-                },
-            ],
-        },
-    ],
-}
+      ],
+    },
+  ],
+};
